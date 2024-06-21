e50afe3a9eb116edfad7c70fd50fff4c1f7da1a9
jarnsorm <gexagen666@gmail.com>
2024-06-21 13:35:11 +0300

    меня ломает /doc_delete/

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import os
 import aiofiles
 import uvicorn
-from fastapi import FastAPI, UploadFile, status
+from fastapi import FastAPI, UploadFile, status, HTTPException
 from fastapi.responses import FileResponse, JSONResponse
 from sqlalchemy import select, insert
 from sqlalchemy.exc import IntegrityError
@@ -13,10 +13,18 @@ app = FastAPI()
 
 
 async def doc_id_to_path(doc_id: int) -> str:
-    async with async_connection() as conn:
-        stmt = select(Documents.path).filter(Documents.id == doc_id)
-        result = await conn.execute(stmt)
-    return result.scalar()
+    print("starting 'doc_id_to_path'")
+    try:
+        async with async_connection() as conn:
+            stmt = select(Documents.path).filter(Documents.id == doc_id)
+            result = await conn.execute(stmt)
+            path = result.scalar()
+            if not path:
+                raise ValueError(f"File path not found for doc_id={doc_id}")
+            return path
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        raise
 
 
 @app.get('/')
@@ -48,25 +56,27 @@ async def upload_doc(file: UploadFile) -> JSONResponse:
 
 @app.delete('/doc_delete/')
 async def doc_delete(doc_id: int) -> JSONResponse:
-    """Удаление файла, удаление данных о нем из БД"""
+    """Удаление файла и данных о нем из БД"""
     try:
-        res = await doc_id_to_path(doc_id)
-        os.remove(res)
+        file_path = await doc_id_to_path(doc_id)
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"File not found at {file_path}")
         async with async_connection() as conn:
             obj = await conn.get(Documents, doc_id)
+            if obj is None:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Document with id={doc_id} not found")
             await conn.delete(obj)
             await conn.commit()
-        return JSONResponse(content={'message': 'file has been deleted'},
+        return JSONResponse(content={'message': 'File and data have been deleted'},
                             status_code=status.HTTP_200_OK)
-    except ValueError:
-        return JSONResponse(content={'message': 'wrong id'},
-                            status_code=status.HTTP_404_NOT_FOUND)
-    except IntegrityError:
-        return JSONResponse(content={'message': 'database integrity error'},
-                            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    except Exception as e:
-        return JSONResponse(content={'message': f'An error occurred: {str(e)}'},
-                            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    except HTTPException as http_exc:
+        raise http_exc
+    # except Exception as e:
+    #     print(e)
+    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+    #                         detail=f"Document not found")
 
 
 @app.post('/doc_analyse/')
--- tests/test_1.py
+++ tests/test_1.py
@@ -1,10 +1,70 @@
+import asyncio
+
+import pytest
+import os
 from tests.config import SERVICE_URL
 from tests.classes import Response
-
+from fastapi.testclient import TestClient
+from httpx import AsyncClient, ASGITransport
+from app.main import app
 import requests
+from db.models import Documents
+from sqlalchemy import select, insert
+from db.data import async_connection
+
+client = AsyncClient(transport=ASGITransport(app))
+
+file_name = "test_upload_image.jpg"
+file_path = f"Documents/{file_name}"
 
 
-def test_connection():
-    response = requests.get(SERVICE_URL)
-    test_obj = Response(response)
-    test_obj.assert_status(200)
+async def doc_test_id(name):
+    async with async_connection() as conn:
+        stmt = select(Documents.id).filter(Documents.path == f"Documents/{name}")
+        result = await conn.execute(stmt)
+        doc_id = result.scalar()
+        print(doc_id)
+        return doc_id
+
+
+@pytest.mark.asyncio
+async def test_upload_valid_file():
+    """Тест на загрузку файла с допустимым расширением"""
+    with open(file_name, "wb") as f:
+        f.write(b"test image content")
+    with open(file_name, "rb") as f:
+        response = await client.post(f"{SERVICE_URL}/upload_doc/", files={"file": (file_name, f, "image/jpeg")})
+    assert response.status_code == 201
+    assert response.json() == {'message': f'file "{file_name}" has been uploaded'}
+
+
+@pytest.mark.asyncio
+async def test_upload_invalid_file():
+    """Тест на загрузку файла с недопустимым расширением"""
+    with open("test_document.txt", "wb") as f:
+        f.write(b"test document content")
+    with open("test_document.txt", "rb") as f:
+        response = await client.post(f"{SERVICE_URL}/upload_doc/", files={"file": ("test_document.txt", f, "text/plain")})
+    assert response.status_code == 400
+    assert response.json() == {'message': 'wrong format of file'}
+
+
+@pytest.mark.asyncio
+async def test_doc_delete():
+    """Удаление файла"""
+    # doc_id = await doc_test_id(file_name)
+    doc_id = 80
+
+    async with AsyncClient(app=app, base_url=f'{SERVICE_URL}') as clnt:
+        response = await clnt.delete(f"/doc_delete/?doc_id={doc_id}")
+
+    assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
+    assert not os.path.exists(file_path), f"File {file_path} still exists after deletion"
+
+    # async with async_connection() as conn:
+    #     stmt = select(Documents).filter(Documents.id == doc_id)
+    #     result = await conn.execute(stmt)
+    #     document = result.scalar()
+    #     assert document is None, f"Document with id {doc_id} still exists in the database"
+
+
